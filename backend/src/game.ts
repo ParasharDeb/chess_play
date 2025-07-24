ca2b18b7922f86381293b0923aec359afd103ddd
Parashar Deb <parashardeb11@gmail.com>
2025-07-24 20:45:30 +0530

    checking if this is my move

--- backend/src/game.ts
+++ backend/src/game.ts
@@ -23,6 +23,12 @@ export class Game {
         }))
     }
     makeMove(socket:WebSocket,move:{from:string,to:string})  {
+        if(this.board.moves.length%2===0 && socket!=this.player1){
+            return
+        }
+        if(this.board.move.length%2===1 && socket!=this.player2){
+            return
+        }
         try{
             this.board.move(move)
         }
