16f667393368928a7d5c31ae7a7383fde426f936
ParasharDeb <parashardeb11@gmail.com>
2026-01-25 19:09:13 +0530

    restarted working on this repo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
-
+import { prisma } from "@repo/db"
 import { WebSocketServer } from 'ws';
 import { GameManager } from './gamemanager';
 
 const wss = new WebSocketServer({ port: 8080 });
 const manager = new GameManager();
 
-wss.on('connection', function connection(ws) {
+wss.on('connection',async function connection(ws) {
+    const user = await prisma.user.create({
+        data:{
+            email:Math.random().toString(),
+            name:Math.random().toString()
+        }
+    })
+    console.log(user)
     manager.addUser(ws);
     ws.on('close', () => manager.removeUser(ws));
 });
