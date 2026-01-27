503316b3d79b2ea3450ca5199a56f635c34d2d26
ParasharDeb <parashardeb11@gmail.com>
2026-01-27 20:50:05 +0530

    fresh start

--- frontend/app/waiting/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function WaitingPage() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-xl">Finding opponent...</h1>
-    </div>
-  );
-}
