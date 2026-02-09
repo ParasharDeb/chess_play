f0215a48b1b865c6150da8bb968d4c3ef840622a
ParasharDeb <parashardeb11@gmail.com>
2026-02-10 01:08:46 +0530

    changed db schema

--- database/src/index.ts
+++ database/src/index.ts
@@ -7,12 +7,27 @@ export async function connectDB() {
   await mongoose.connect("mongodb://localhost:27017")
   console.log("Mongo connected");
 }
+const winnerEnum = ["white", "black", "draw","none"];
 const User = new Schema({
-
-  name: {type:String,required:true},
+  name: {type:String,required:true,unique:true},
   email: {type: String, unique: true},
   password: {type:String,required:true},
   rating: {type:Number,default:800}
 });
+const Games= new  Schema({
+  whiteplayer:{
+    type:Schema.Types.ObjectId,
+    ref:"User",
+    required:true
+  },
+  blackplayer:{
+    type:Schema.Types.ObjectId,
+    ref:"User",
+    required:true
+  },
+  moves:[],
+  winner:{type:String,enum:winnerEnum,default:"none"}
+})
 
-export const UserModel = mongoose.model('users', User);
+export const UserModel = mongoose.model('User', User);
+export const GamesModel=mongoose.model('games',Games)
